30f813133500d5e1c82f078cfc9b88facb23a724
Yonny Zohar <yonnyzohar@gmail.com>
2026-03-20 17:14:03 +0000

    wip

--- node_modules/@pixi-spine/all-4.0/index.d.ts
+++
@@ -1,5 +0,0 @@
-
-export * from "@pixi-spine/base";
-export * from "@pixi-spine/runtime-4.0";
-
-export { }
--- node_modules/@pixi-spine/loader-4.0/global.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace GlobalMixins
-{
-    interface LoaderResource {
-        spineData?: import('@pixi-spine/runtime-4.0').SkeletonData;
-    }
-}
